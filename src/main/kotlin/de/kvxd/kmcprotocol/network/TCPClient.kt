8eb70adcb33e92ec4659b73df7436845888b9529
meo209 <redslyice@outlook.de>
2025-03-12 23:47:16 +0100

    feat: Simple client

---
+++ src/main/kotlin/de/kvxd/kmcprotocol/network/TCPClient.kt
@@ -0,0 +1,30 @@
+package de.kvxd.kmcprotocol.network
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class TCPClient(private val host: String, private val port: Int) {
+
+    private val selectorManager = ActorSelectorManager(Dispatchers.IO)
+    private var socket: Socket? = null
+    private var output: ByteWriteChannel? = null
+
+    suspend fun connect() {
+        socket = aSocket(selectorManager).tcp().connect(host, port)
+        output = socket?.openWriteChannel(autoFlush = true)
+    }
+
+    suspend fun sendPacket(packet: ByteArray) {
+        withContext(Dispatchers.IO) {
+            output?.writeFully(packet)
+        }
+    }
+
+    suspend fun close() = withContext(Dispatchers.IO) {
+        socket?.close()
+        selectorManager.close()
+    }
+}
--- src/test/kotlin/ClientTest.kt
+++ src/test/kotlin/ClientTest.kt
@@ -1,13 +1,36 @@
-import kotlinx.coroutines.DEBUG_PROPERTY_NAME
-import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+import de.kvxd.kmcprotocol.MinecraftProtocol
+import de.kvxd.kmcprotocol.network.TCPClient
+import de.kvxd.kmcprotocol.registry.PacketRegistry
+import de.kvxd.kmcprotocol.serialization.PacketSerializer
 import kotlinx.coroutines.runBlocking
 import kotlin.test.Test
 
 class ClientTest {
 
+    private val protocol = MinecraftProtocol()
+
     @Test
     fun testConnection() = runBlocking {
+        val client = TCPClient("localhost", 25565)
 
+        client.connect()
+
+        val packet = HandshakePacket(
+            protocolVersion = 754,
+            serverAddress = "localhost",
+            serverPort = 25565,
+            nextState = 1
+        )
+
+        protocol.registry = PacketRegistry.create(protocol) {
+            registerPacket(HandshakePacket::class)
+        }
+
+        val serialized = PacketSerializer.serialize(protocol, packet)
+
+        client.sendPacket(serialized)
+
+        client.close()
     }
 
 }

1ea988b68088f6bea4805f41741d42508bfafd49
meo209 <redslyice@outlook.de>
2025-03-12 23:47:53 +0100

    refactor: Removed @Serializable on integers in HandshakePacket.kt

--- src/test/kotlin/HandshakePacket.kt
+++ src/test/kotlin/HandshakePacket.kt
@@ -1,7 +1,7 @@
+
 import de.kvxd.kmcprotocol.Direction
 import de.kvxd.kmcprotocol.MinecraftPacket
 import de.kvxd.kmcprotocol.ProtocolState
-import de.kvxd.kmcprotocol.datatypes.varint.VarIntSerializer
 import de.kvxd.kmcprotocol.registry.PacketMetadata
 import kotlinx.serialization.Serializable
 
@@ -12,10 +12,8 @@ import kotlinx.serialization.Serializable
     state = ProtocolState.HANDSHAKE
 )
 data class HandshakePacket(
-    @Serializable(with = VarIntSerializer::class)
     val protocolVersion: Int,
     val serverAddress: String,
     val serverPort: Short,
-    @Serializable(with = VarIntSerializer::class)
     val nextState: Int
 ) : MinecraftPacket

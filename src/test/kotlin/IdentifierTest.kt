2812705a7d6b26584b5f9ed0f18c85b7285f8bdf
meo209 <redslyice@outlook.de>
2025-03-13 22:29:46 +0100

    test: Identifier

---
+++ src/test/kotlin/IdentifierTest.kt
@@ -0,0 +1,40 @@
+import de.kvxd.kmcprotocol.datatypes.Identifier
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class IdentifierTest {
+
+    @Test
+    fun `test Identifier creation with namespace and path`() {
+        val identifier = Identifier.of("customnamespace", "custompath")
+        assertEquals("customnamespace:custompath", identifier.toString())
+    }
+
+    @Test
+    fun `test Identifier creation with default namespace`() {
+        val identifier = Identifier.of("custompath")
+        assertEquals("minecraft:custompath", identifier.toString())
+    }
+
+    @Test
+    fun `test Identifier creation from string`() {
+        val identifier = Identifier.fromString("customnamespace:custompath")
+        assertEquals("customnamespace:custompath", identifier.toString())
+    }
+
+    @Test
+    fun `test Identifier with too long string`() {
+        val longPath = "a".repeat(32767 - "customnamespace:".length + 1)
+        assertFailsWith<IllegalArgumentException> {
+            Identifier.of("customnamespace", longPath)
+        }
+    }
+
+    @Test
+    fun `test Identifier without namespace separator`() {
+        val identifier = Identifier.of("custompath")
+        assertEquals("minecraft:custompath", identifier.toString())
+    }
+
+}
